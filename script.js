96d72e7e8b319b35b98e300314ab724032d8c173
BD5H13LD <khalidhasan2ktwo4@gmail.com>
2025-03-13 21:50:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Button Functionality
+document.getElementById("homeBtn").addEventListener("click", () => {
+  alert("Home button clicked!");
+});
+
+document.getElementById("aboutBtn").addEventListener("click", () => {
+  alert("About button clicked!");
+});
